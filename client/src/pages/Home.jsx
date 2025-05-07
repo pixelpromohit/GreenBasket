a9d334ead2edd99289d3f4cbddc64bb1b41644c0
Mohit Ramchandani <ramchandanimohit771@gmail.com>
2025-05-07 19:56:13 +0530

    Categories  (1:21:00)

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import MainBanner from '../components/MainBanner'
+import Categories from '../components/Categories';
+import Bestsellers from '../components/Bestsellers';
 
 const Home = () => {
   return (
     <div className='mt-10'>
       <MainBanner/>
+      <Categories/>
+      <Bestsellers/>
     </div>
   )
 }
