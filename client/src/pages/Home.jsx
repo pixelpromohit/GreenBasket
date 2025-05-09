039a1a9d0911a65bc56cdba6252265ae21b97fff
Mohit Ramchandani <ramchandanimohit771@gmail.com>
2025-05-09 17:52:35 +0530

    newsletter

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import MainBanner from '../components/MainBanner'
 import Categories from '../components/Categories';
 import Bestsellers from '../components/Bestsellers';
 import BottomBanner from '../components/BottomBanner';
+import NewsLetter from '../components/NewsLetter';
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Categories/>
       <Bestsellers/>
       <BottomBanner/>
+      <NewsLetter/>
     </div>
   )
 }
