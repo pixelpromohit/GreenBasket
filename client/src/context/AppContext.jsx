53510aa58c8c2d679fae5d548e4e10b72410d42e
Mohit Ramchandani <ramchandanimohit771@gmail.com>
2025-05-11 15:44:03 +0530

    all product page

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -15,6 +15,7 @@ export const AppContextProvider = ({children}) => {
     const [showUserLogin, setShowUserLogin] = useState(false)
     const [products, setProducts] = useState([])
     const [cartItems, setCartItems] = useState({})
+    const [searchQuery, setSearchQuery] = useState({})
 
     // Fetch all products
     const fetchProducts = async () => {
@@ -60,7 +61,7 @@ export const AppContextProvider = ({children}) => {
     }, [])
 
     const value = {navigate, user, setUser, setIsSeller, isSeller,
-        showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItem, removeFromCart, cartItems
+        showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItem, removeFromCart, cartItems, searchQuery, setSearchQuery
     }
     return <AppContext.Provider value={value}>
         {children}
