bc814bcc5210fbc7c35315fb7ba2de9799415353
Mohit Ramchandani <ramchandanimohit771@gmail.com>
2025-05-27 11:18:37 +0530

    cart icon functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { useAppContext } from '../context/AppContext'
 
 const Navbar = () => {
     const [open, setOpen] = React.useState(false)
-    const {user, setUser, setShowUserLogin, navigate, setSearchQuery, searchQuery} = useAppContext()
+    const {user, setUser, setShowUserLogin, navigate, setSearchQuery, searchQuery, getCartCount, getCartAmount} = useAppContext()
 
     const logout = async () => {
         setUser(null)
@@ -39,7 +39,7 @@ const Navbar = () => {
 
             <div onClick={()=> navigate("/cart")} className="relative cursor-pointer">
                 <img src={assets.nav_cart_icon} alt='cart' className='w-6 opacity-80'/>
-                <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
+                <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
             </div>
 
             {/* if user is not logged in then show the login button else show profile icon*/}
@@ -58,9 +58,17 @@ const Navbar = () => {
             )}
         </div>
 
-        <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
-            <img src={assets.menu_icon} alt="menu" />            
-        </button>
+        <div className='flex items-center gap-6 sm:hidden'>
+            <div onClick={()=> navigate("/cart")} className="relative cursor-pointer">
+                <img src={assets.nav_cart_icon} alt='cart' className='w-6 opacity-80'/>
+                <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
+            </div>
+
+            <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="">
+                <img src={assets.menu_icon} alt="menu" />            
+            </button>
+        </div>
+
 
         { open && (
             <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
