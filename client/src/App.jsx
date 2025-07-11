0094d4b4eefdf62f3fa02ad23d73e80d823d264a
mohit <ramchandanimohit771@gmail.com>
2025-07-11 21:38:43 +0530

    Frontend Complete <3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,9 @@ import AddAddress from './pages/AddAddress'
 import MyOrders from './pages/MyOrders'
 import SellerLogin from './components/seller/SellerLogin'
 import SellerLayout from './pages/seller/SellerLayout'
+import ProductList from './pages/seller/ProductList'
+import AddProduct from './pages/seller/AddProduct'
+import Orders from './pages/seller/Orders'
 
 const App = () => {
 
@@ -38,7 +41,9 @@ const App = () => {
           <Route path='/add-address' element={<AddAddress/>}/>
           <Route path='/my-orders' element={<MyOrders/>}/>
           <Route path='/seller' element={isSeller ? <SellerLayout/> : <SellerLogin/>}>
-
+            <Route index element={isSeller ? <AddProduct/> : null}/>
+            <Route path='product-list' element={<ProductList/>}/>
+            <Route path='orders' element={<Orders/>}/>
           </Route>
         </Routes>
       </div>
--- client/src/pages/seller/SellerLayout.jsx
+++ client/src/pages/seller/SellerLayout.jsx
@@ -9,7 +9,7 @@ const SellerLayout = () => {
     const sidebarLinks = [
         { name: "Add Product", path: "/seller", icon: assets.add_icon },
         { name: "Product List", path: "/seller/product-list", icon: assets.product_list_icon },
-        { name: "Orders", path: "/sellers/orders", icon: assets.order_icon },
+        { name: "Orders", path: "/seller/orders", icon: assets.order_icon },
     ];
 
     const logout = async () => {
@@ -28,7 +28,7 @@ const SellerLayout = () => {
                 </div>
             </div>
             <div className="flex">
-               <div className="md:w-64 w-16 border-r h-[550px] text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
+               <div className="md:w-64 w-16 border-r h-[95vh] text-base border-gray-300 pt-4 flex flex-col">
                     {sidebarLinks.map((item) => (
                         <NavLink to={item.path} key={item.name} end={item.path === "/seller"}
                             className={({isActive})=>`flex items-center py-3 px-4 gap-3 
